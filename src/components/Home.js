60f09a303b2c6574e9cfef5aabf100063e46f86f
sardar-ali <alisardar465@gmail.com>
2025-02-22 18:00:37 +0400

    feat: add application version

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 // import { Link } from 'react-router-dom';
 
 function Home() {
-    return (
-        <div className='flex justify-center align-center flex-column home'>
-           <h1>React App - Welcome!</h1>
-           <p>Learn Jenkins</p>
-            {/* <Link to="/products"> */}
-                <button>Go To Product Screen <img src="/images/arrow.jpg" alt='img' /></button>
-            {/* </Link> */}
-        </div>
-    )
+  return (
+    <div className="flex justify-center align-center flex-column home">
+      <h1>React App - Welcome!</h1>
+      <p>Learn Jenkins</p>
+      {/* <Link to="/products"> */}
+      <button>
+        Go To Product Screen <img src="/images/arrow.jpg" alt="img" />
+      </button>
+      <p>{process.env.APP_VERSION ?? "1.0.0"} v</p>
+      {/* </Link> */}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
